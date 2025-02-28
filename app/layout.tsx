d7a72e4662f60c0a8160f44c3a268a452b63129c
SabooTrueValue <165014077+SabooTrueValue@users.noreply.github.com>
2025-02-28 09:51:09 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Saboo Insurance App",
+  description: "Created for saboo insurance agents",
+};
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${font.className}  antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
